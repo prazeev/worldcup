a7cebc5a55efdad9c17fbc1bf03c79f54f4a589c
Basudev Paudel <prazeev@gmail.com>
2018-06-13 15:51:50 +0545

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @Email:  prazeev@gmail.com
  * @Filename: index.js
  * @Last modified by:   prazeev
- * @Last modified time: 2018-06-13T14:11:28+05:45
+ * @Last modified time: 2018-06-13T15:51:30+05:45
  * @Copyright: Copyright 2018, Bashudev Poudel
  */
 var express = require('express')
@@ -61,5 +61,5 @@ app.get("/dashboard",function(req, res) {
 })
 
 
-app.listen(8262)
+app.listen(80)
 console.log("Server Started on port 8262")
