b195bbfd8ddb23e2063c91e9a18f9778c4da3dc8
Basudev Paudel <prazeev@gmail.com>
2018-06-14 14:33:19 +0545

    Home Page

--- routes/Frontend.js
+++ routes/Frontend.js
@@ -4,7 +4,7 @@
  * @Email:  prazeev@gmail.com
  * @Filename: Frontend.js
  * @Last modified by:   prazeev
- * @Last modified time: 2018-06-14T14:30:06+05:45
+ * @Last modified time: 2018-06-14T14:33:06+05:45
  * @Copyright: Copyright 2018, Bashudev Poudel
  */
  var express = require('express')
@@ -41,14 +41,10 @@
        next()
      }
    }
-   router.get("/", function(req, res) {
-     if(req.user == undefined) {
-       res.render("frontend/homepage.ejs", {
-         user: req.user
-       })
-     } else {
-       res.redirect("/play")
-     }
+   router.get("/", checkAuth, function(req, res) {
+     res.render("frontend/homepage.ejs", {
+       user: req.user
+     })
    })
    router.get("/play/login", passport.authenticate('facebook'), function(req, res) {
      res.redirect("/play")
