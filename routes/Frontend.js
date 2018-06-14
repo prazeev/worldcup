4a1c0ee60f6bd9df2d860b19a2692985e58c6ff6
Basudev Paudel <prazeev@gmail.com>
2018-06-14 14:48:48 +0545

    Home Page

--- routes/Frontend.js
+++ routes/Frontend.js
@@ -4,7 +4,7 @@
  * @Email:  prazeev@gmail.com
  * @Filename: Frontend.js
  * @Last modified by:   prazeev
- * @Last modified time: 2018-06-14T14:46:09+05:45
+ * @Last modified time: 2018-06-14T14:48:17+05:45
  * @Copyright: Copyright 2018, Bashudev Poudel
  */
  var express = require('express')
@@ -19,7 +19,7 @@
  passport.use(new FacebookStrategy({
      clientID: 1626068404365914,
      clientSecret: "c9716b4e59fb4f5a503a9469329648a2",
-     callbackURL: "http://worldcup.bizpati.com/"
+     callbackURL: "http://worldcup.bizpati.com/user/callback"
    },
    function(accessToken, refreshToken, profile, done) {
      done(null, profile)
@@ -49,6 +49,9 @@
      }
    })
    router.get("/play/login", passport.authenticate('facebook'), function(req, res) {
+     res.redirect("/play")
+   })
+   router.get("/user/callback", function(req, res) {
      res.redirect("/play")
    })
    router.get("/play", checkAuth, function(req, res) {
