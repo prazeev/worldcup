a0ed1aff7040516f79d094c7793047294c74b74a
Basudev Paudel <prazeev@gmail.com>
2018-06-16 15:17:37 +0545

    Minor Fixes

--- routes/Frontend.js
+++ routes/Frontend.js
@@ -4,7 +4,7 @@
  * @Email:  prazeev@gmail.com
  * @Filename: Frontend.js
  * @Last modified by:   prazeev
- * @Last modified time: 2018-06-16T14:42:02+05:45
+ * @Last modified time: 2018-06-16T15:16:11+05:45
  * @Copyright: Copyright 2018, Bashudev Poudel
  */
  var express = require('express')
@@ -101,11 +101,28 @@
 
    })
    router.get("/profile", checkAuth, function(req, res) {
-     dbo.collection("users").find({
-       fb_id: req.user.id
-     }).toArray(function(err, result) {
+     dbo.collection("prediction")aggregate([
+       {
+         $match: {
+           fb_id: req.user.id
+         }
+       }, {
+         $lookup: {
+             from: 'score',
+             localField: 'score',
+             foreignField: '_id',
+             as: 'scores'
+         }
+       }, {
+         $lookup: {
+             from: 'users',
+             localField: 'fb_id',
+             foreignField: 'fb_id',
+             as: 'user'
+         }
+       }
+     ]).toArray(function(err, result) {
        if (err) throw err;
-       console.log(result);
        res.render("frontend/profile.ejs", {
          data: result[0],
          user: req.user
--- routes/FrontendApi.js
+++ routes/FrontendApi.js
@@ -4,7 +4,7 @@
  * @Email:  prazeev@gmail.com
  * @Filename: Api.js
  * @Last modified by:   prazeev
- * @Last modified time: 2018-06-15T19:55:39+05:45
+ * @Last modified time: 2018-06-16T14:47:35+05:45
  * @Copyright: Copyright 2018, Bashudev Poudel
  */
  var express = require('express')
