43bf35786b85d1495f6a8429287427513d2f1772
Basudev Paudel <prazeev@gmail.com>
2018-06-16 16:16:51 +0545

    Minor Fixes

--- routes/Frontend.js
+++ routes/Frontend.js
@@ -4,7 +4,7 @@
  * @Email:  prazeev@gmail.com
  * @Filename: Frontend.js
  * @Last modified by:   prazeev
- * @Last modified time: 2018-06-16T15:25:16+05:45
+ * @Last modified time: 2018-06-16T16:16:10+05:45
  * @Copyright: Copyright 2018, Bashudev Poudel
  */
  var express = require('express')
@@ -123,9 +123,14 @@
        }
      ]).toArray(function(err, result) {
        if (err) throw err;
-       res.render("frontend/profile.ejs", {
-         data: result,
-         user: req.user
+       dbo.collection("users").find({
+         fb_id: req.user.id
+       }).toArray(function(uerror, uresult) {
+         res.render("frontend/profile.ejs", {
+           data: result,
+           user: req.user,
+           userdata: uresult[0]
+         })
        })
      });
    });
