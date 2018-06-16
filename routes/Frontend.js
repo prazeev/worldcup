4d9e453e6d5930951d41b455f8f296162cc25b93
Basudev Paudel <prazeev@gmail.com>
2018-06-16 14:37:06 +0545

    Added banners

--- routes/Frontend.js
+++ routes/Frontend.js
@@ -4,7 +4,7 @@
  * @Email:  prazeev@gmail.com
  * @Filename: Frontend.js
  * @Last modified by:   prazeev
- * @Last modified time: 2018-06-16T14:35:11+05:45
+ * @Last modified time: 2018-06-16T14:36:22+05:45
  * @Copyright: Copyright 2018, Bashudev Poudel
  */
  var express = require('express')
